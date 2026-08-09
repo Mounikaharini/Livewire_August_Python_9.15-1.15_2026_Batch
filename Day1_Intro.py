89eb0fb8873b407bef85ca768375da896755e79e
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-08-09 13:14:18 +0530

    Add files via upload

---
+++ Day1_Intro.py
@@ -0,0 +1,67 @@
+'''
+print() function
+The print() function in python is used to
+display output in the screen
+Syntax : print(values)
+
+#task 1
+print(10)
+print("hi")
+print(10,end="\t")
+print(10)
+print("10\n10\n10")
+
+a=10
+print(a)
+a=20
+print(a)
+A=30
+print(A)
+
+help("keywords")
+
+a = 10
+print(a)
+print(type(a))
+
+a = 10.09754
+print(a)
+print(type(a))
+
+#a = 10+2i
+print(a)
+print(type(a))
+
+a = True
+print(a)
+print(type(a))
+
+a = "hi , hello [10][10.369]"
+print(a)
+print(type(a))
+
+a = [1,1,1.0,'hi',True,[1,2,3]]
+print(a)
+print(type(a))
+
+a = (1,1,1.0,'hi',True,[1,2,3])
+print(a)
+print(type(a))
+
+a = {1,1,1.0,'hi',False,0}
+print(a)
+print(type(a))
+
+#remove duplicates in a list using set data type
+b = [1,1,2,3,2,1,3,1,2]
+print(set(b))
+
+a = {1:"Mounika",2:"Prathika",1:"Vinodhini"}
+print(a)
+print(type(a))
+'''
+
+
+
+
+
