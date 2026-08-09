739985bdb258932e1a76b1eeecbed46e7c622342
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-08-09 13:14:38 +0530

    Add files via upload

---
+++ Day1_operators.py
@@ -0,0 +1,38 @@
+#Arithmetic operator
+
+a = 10
+b = 20
+c = a + b
+print(c)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(2**10)
+
+#Assignment operator
+
+a = 10
+print(a)
+a+=20
+print(a)
+a-=20
+print(a)
+a*=2
+print(a)
+a/=3
+print(a)
+a//=1
+print(a)
+a%=5
+print(a)
+a**=5
+print(a)
+
+
+
+
+
+
